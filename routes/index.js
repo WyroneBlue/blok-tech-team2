953179b431442b0a0da9f30735a54a98983d9a97
WyroneBlue <wyroneblue@gmail.com>
2022-03-27 00:47:51 +0100

    added messages route

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const home  = require('./home');
 const restaurants  = require('./restaurants');
+const messages  = require('./messages');
 
 router.use('/', home);
 router.use('/restaurants', restaurants);
+router.use('/messages', messages);
 
 
 module.exports = router;
