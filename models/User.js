104574e9b156123b49503c727bf9ba3195681454
WyroneBlue <wyroneblue@gmail.com>
2022-03-26 21:34:37 +0100

    added new models

---
+++ models/User.js
@@ -0,0 +1,17 @@
+//Require Mongoose
+const mongoose = require('mongoose');
+
+//Define a schema
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+	name: String,
+	email: String,
+	img: {type: String, default: 'https://picsum.photos/200'},
+	reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }],
+	created: {type: Date, default: Date.now},
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
