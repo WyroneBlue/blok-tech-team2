f74bdc25b8a1619fbdb7bc4d5acfc87d354d805a
WyroneBlue <wyroneblue@gmail.com>
2022-03-26 21:35:11 +0100

    added restaurants to routes/index

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const home  = require('./home');
+const restaurants  = require('./restaurants');
 
 router.use('/', home);
+router.use('/restaurants', restaurants);
+
 
 module.exports = router;
