5ff041715ede3ba9f51570e053fcf1a0dee446ca
WyroneBlue <wyroneblue@gmail.com>
2022-03-28 13:59:48 +0200

    added messages controller functions for routes

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,10 +1,16 @@
-const index = (req, res) => {
+const { User, Chat } = require('../models');
+
+const index = async(req, res) => {
 	const page = {
-		title: "Matched Users"
+		title: "All messages"
 	};
-    
+
+	const user = await User.findOne({ username: 'ymaroblue'}).lean();
+	const chats = await Chat.find({ $or: [{inviter: user}, {receiver: user}] })
+	.populate('inviter').populate('receiver').lean();
 	res.status(200).render('messages/index', { 
 		page: page,
+		users: chats,
 	});
 };
 
@@ -13,12 +19,109 @@ const chat = (req, res) => {
 		title: "Chat"
 	};
     
-	res.status(200).render('messages/chat', { 
-		page: page,
+	const promises = [
+		User.findOne({ username: 'ymaroblue' }).lean(), 
+		User.findOne({ username: req.params.username }).lean(), 
+	];
+
+	Promise.all(promises)
+	.then(async result => {
+		const [inviter, receiver] = result;
+		
+		const chat = await Chat.findOne({ inviter: inviter, receiver: receiver }).populate('receiver')
+		.populate({ 
+			path: 'history.user',
+			model: User
+		})	
+		.lean()
+		console.log(chat);
+		let accepted = false;
+		if(chat && chat.accepted){
+			accepted = true;
+		}
+
+		res.status(200).render('messages/chat', { 
+			page: page,
+			chat: chat,
+			accepted: accepted,
+		});
+	})
+
+
+};
+
+const create = async(req, res) => {
+    
+	const promises = [
+		User.findOne({ username: 'ymaroblue' }).lean(), 
+		User.findOne({ username: req.params.username }).lean(), 
+	];
+
+	Promise.all(promises)
+	.then(async result => {
+		const [inviter, receiver] = result;
+
+		// console.log(inviter);
+		// console.log(receiver);
+
+		const checkChat = await Chat.findOne({ inviter: inviter, receiver: receiver });
+		if(checkChat){
+			res.redirect(`/messages/${receiver.username}/chat`);
+		} else {
+
+			const form = {
+				inviter: inviter,
+				receiver: receiver,
+			};
+
+			const chat = new Chat(form);
+		
+			chat.save((err) => {
+				if (err) return handleError(err);
+				res.redirect(`/messages/${receiver.username}/chat`);
+			});
+		}
 	});
+};
+
+const update = async (req, res) => {
+    
+	const promises = [
+		User.findOne({ username: 'ymaroblue' }).lean(), 
+		User.findOne({ username: req.params.username }).lean(), 
+	];
+	// console.log(req.body);
+	Promise.all(promises)
+	.then(async result => {
+		const [inviter, receiver] = result;
+
+		const chat = await Chat.findOne({ inviter: inviter, receiver: receiver})
+		if(chat && chat.accepted){
+			const input = req.body;
+			const newMessage = {
+				message: input.msg,
+				date: input.date,
+				user: inviter,
+			}
+			chat.history.push(newMessage);
+			chat.save((err) => {
+				if (err) return handleError(err);
+				res.send();
+			});
+		} else {
+			res.send('not_accepted');
+		}
+
+		// await Chat.findOneAndUpdate(
+		// 	{ inviter: inviter, receiver: receiver }, 
+		// 	{ $push: { history: newMessage }}
+		// );
+	})
 };
 
 module.exports = {
 	index: index,
 	chat: chat,
+	create: create,
+	update: update,
 };
