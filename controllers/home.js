f40dccb60e49f69593fda28429729ada163b6092
Rick van Dijken <90160841+rvd98@users.noreply.github.com>
2022-03-28 16:43:17 +0200

    Styled matching cards
    Edited matching cards

--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,14 @@
-const index = (req, res) => {
+const { Restaurant } = require('../models');
+
+const index = async (req, res) => {
 	const page = {
 		title: "Home"
 	};
-    
+	const restaurants = await Restaurant.find().lean();
+	console.log(restaurants);
 	res.status(200).render('home', { 
 		page: page,
+		restaurants: restaurants
 	});
 };
 
