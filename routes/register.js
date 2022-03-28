38636f93828b688b12940a1bb7dfa2379ad2db9a
WyroneBlue <wyroneblue@gmail.com>
2022-03-28 16:19:31 +0200

    cleanup

--- routes/register.js
+++ routes/register.js
@@ -1,14 +1,8 @@
-
-
 const express = require('express');
 const router = express.Router();
 const authorizationController = require("../controllers/authorization");
 
-// router.get('/', {name}.{functie);
-// router.get('/nog-een-route', {name}.{functie);
 router.get('/', authorizationController.register);
-
 router.post('/', authorizationController.saveUser);
-
 
 module.exports = router;
