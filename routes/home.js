730b3f43f30ae0fbabf929bd0749c6711e9b821d
WyroneBlue <wyroneblue@gmail.com>
2022-03-28 16:22:27 +0200

    moved login routes to login controller

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const homeController = require("../controllers/home");
 
 router.get('/', homeController.index);
-router.get('/login', homeController.login);
 router.get('/welkom', homeController.welkom);
 
 module.exports = router;
