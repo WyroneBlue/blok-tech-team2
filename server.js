49cfa7ac62aa1f2fd612714b2b2767b2763fd851
WyroneBlue <wyroneblue@gmail.com>
2022-03-30 14:38:51 +0200

    added sending message to correct chat

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const IO_PORT = process.env.IO_PORT || 8080;
 const session = require('express-session')
-
 const io = require('socket.io')(IO_PORT, {
 	cors: {
 		origin: "*"
@@ -23,12 +22,13 @@ const urlencodedParser = bodyParser.urlencoded({ extended: true });
 app.use(bodyParser.json());
 
 // sessions
-app.use(session({
+const useSession = session({
 	secret: 'keyboard cat',
 	resave: false,
 	saveUninitialized: true
-}))
-  
+})
+app.use(useSession);
+
 // Routes
 const routes = require("./routes");
 
@@ -48,16 +48,22 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 // Use Routes
 app.use('/', urlencodedParser, routes);
 
+// Websockets
+io.on('connection', socket => {
+	socket.on('join-chat', (name) => {
+		socket.join(name);
+	})
+
+	socket.on('new-msg-sent', chat => {
+		socket.to(chat.name).emit('new-msg', chat.msg);
+	})
+})
+
 app.listen(PORT, () => {
   	console.log(`Example app listening on port ${PORT}`);
 });
 
-io.on('connection', socket => {
-	// console.log(socket.id);
-	socket.on('new-msg-sent', msg => {
-		socket.broadcast.emit('new-msg', msg);
-	})
-})
+
 
 
 
