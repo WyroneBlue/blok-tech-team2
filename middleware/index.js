eafe59755e4eafaeb57ab67e6bedce2347e788c3
WyroneBlue <wyroneblue@gmail.com>
2022-03-30 14:57:50 +0200

    socket session wrap
    not working properly

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,10 @@
 const { loggedIn, guest } = require('./authorization');
+const sessionWrap  = require('./socket');
 const viewCounter = require('./viewCounter');
 
 module.exports = {
     loggedIn: loggedIn,
     guest: guest,
+    wrap: sessionWrap,
     viewCounter: viewCounter,
 }
