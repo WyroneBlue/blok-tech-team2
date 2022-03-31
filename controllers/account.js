f08246e6686b17f659776a7b6cdcd4dc4b6b3439
Danique Lammertink <danique.lammertink@hva.nl>
2022-03-31 17:23:49 +0200

    Username op profiel

--- controllers/account.js
+++ controllers/account.js
@@ -1,15 +1,20 @@
 const { User } = require('../models');
 let session;
 
-const account = (req, res) => {
+const account = async(req, res) => {
 	session = req.session;
 	const page = {
 		title: "Account"
 	};
-    
+	
+	const user = await User.findOne({ username: session.authUser.username}).lean();
+	const region = await User.findOne({ region: session.authUser.region}).lean();
+	console.log(user);
 	res.status(200).render('profile/account', { 
 		page: page,
 		layout: false,
+		user: user,
+		region: region
 	});
 };
 
