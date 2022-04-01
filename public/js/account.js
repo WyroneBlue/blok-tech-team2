7ea8b41af9d3e219e1c3c4afa896a3df2d87f493
Claudio Bakker <claudiobakker@gmail.com>
2022-04-01 13:53:18 +0200

    fixed button on editform
    fixed button on editform

--- public/js/account.js
+++ public/js/account.js
@@ -1,34 +1,19 @@
 const editButton = document.querySelector("#accountheader #editbutton");
 
-// const closeButton = document.querySelector("button#closeicon");
 const closeButton = document.querySelectorAll("button")[1];
-console.log(closeButton);
+// console.log(closeButton);
 const formEdit = document.querySelector("#hover");
 
+if (closeButton) {
+    closeButton.addEventListener("click", toggleMenu);
+    
+}
 
-closeButton.addEventListener("click", toggleMenu);
-editButton.addEventListener("click", toggleMenu);
+if (editButton) {
+    editButton.addEventListener("click", toggleMenu);
+}
 
 function toggleMenu(){
     formEdit.classList.toggle("open");
 }
 
-// const openButton = document.querySelector('#accountheader img');
-// const closeButton = document.querySelector('#close');
-// const menu = document.querySelector('nav');
-
-// /* We've applied function names as the second parameter of these eventListeners
-// 	However, we'd like to change these to anonymous arrow functions, making the 
-// 	functions closeMenu() and openMenu() oblivious
-// 	Source: https://www.30secondsofcode.org/articles/s/javascript-arrow-function-event-listeners */
-// openButton.addEventListener('click', (event) => {
-// 	// console.log(event.target.nextElementSibling);
-// 	menu.classList.add('is-open')
-// 	// event.target.nextElementSibling.classList.add('is-open')
-// } );
-									 
-// closeButton.addEventListener('click', (event) => {
-// 	menu.classList.remove('is-open')
-// 	// event.target.nextElementSibling.classList.remove('is-open')
-
-// });
