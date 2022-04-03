93fedb99d6667fac3e29e6bd34f28d8dfb3e3345
WyroneBlue <wyroneblue@gmail.com>
2022-04-03 20:57:15 +0200

    cleanup

--- routes/error.js
+++ routes/error.js
@@ -3,11 +3,6 @@ const express = require('express');
 const router = express.Router();
 const errorController = require("../controllers/error");
 
-// router.get('/', {name}.{functie);
-// router.get('/nog-een-route', {name}.{functie);
 router.get('/', errorController.error);
-
-// router.post('/', errorController.saveUser);
-
 
 module.exports = router;
