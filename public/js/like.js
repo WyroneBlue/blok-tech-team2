17a9b6eeeccecc8f0a7453a5e38775f6064431ef
WyroneBlue <wyroneblue@gmail.com>
2022-04-03 21:05:53 +0200

    added frontend dislike functionality

--- public/js/like.js
+++ public/js/like.js
@@ -1,4 +1,5 @@
 const likeButtons = document.querySelectorAll("form button.like");
+const dislikeButtons = document.querySelectorAll("form button.dislike");
 const noMoreCardsSection = document.querySelector("#no-more-cards");
 let cards = [];
 let cardCounter;
@@ -32,6 +33,34 @@ async function saveLike (e) {
 		window.location = '/login';
 	}
 }
+
+async function saveDislike (e) {
+
+	const form = e.parentNode;
+	const formData = new FormData(form);
+    const restaurant_id = formData.get("restaurant_id").trim();
+	const card = form.parentNode.parentNode.parentNode;
+	const route = '/dislikes/save';
+	try {
+		
+		const response = await axios.post(route, {
+			restaurant_id: restaurant_id,
+		});
+		console.log(response);
+		if(response.status === 200){
+			cardCounter--;
+			card.classList.add('swipe-left');
+			card.addEventListener('animationend', (e) => {
+				card.remove();
+			});
+			if(cardCounter === 0){
+				console.log('no more');
+				noMoreCardsSection.classList.add('show')
+			}
+		}
+		return response;
+	} catch (errors) {
+		window.location = '/login';
 	}
 }
 			
@@ -42,5 +71,12 @@ likeButtons.forEach(btn => {
 	btn.addEventListener("click", function(e) {
 		e.preventDefault();
 		saveLike(this);
+	});
+});
+
+dislikeButtons.forEach(btn => {
+	btn.addEventListener("click", function(e) {
+		e.preventDefault();
+		saveDislike(this);
 	});
 });
